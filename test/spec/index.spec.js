d4feb801ef8c6049ff352487f3f9c0e172042f3a
Niv Sardi <xaiki@evilgiggle.com>
2018-04-30 11:57:07 -0300

    tests: details doesn't set fetched
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -196,7 +196,7 @@ describe('butter-redux-provider', () => {
                 state = store.getState()
 
                 expect(state.isFetching).toEqual(false, 'isFetching resolved')
-                expect(state.fetched).toEqual(true, 'fetched resolved')
+
                 expect(state.detail).toEqual(42)
                 expect(state.cache[42]).toExist()
                 expect(state.cache[42]).toIncludeKey('id')
