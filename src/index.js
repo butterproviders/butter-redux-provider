cc695438b3cb54468cbd65c6ef2a50e5bb61c2d3
Niv Sardi <xaiki@evilgiggle.com>
2018-05-01 10:50:37 -0300

    use standard javascript
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/index.js
+++ src/index.js
@@ -1,195 +1,192 @@
-import {createAsyncAction, createReducer} from 'redux-action-tools';
+import {createAsyncAction, createReducer} from 'redux-action-tools'
 const debug = require('debug')('butter-redux-provider')
 
 const hashify = (source, keyFn = (k) => k) => (
-    source.reduce((acc, cur) => (
-        Object.assign(acc, {
-            [keyFn(cur)]: cur
-        })
-    ), {})
+  source.reduce((acc, cur) => (
+    Object.assign(acc, {
+      [keyFn(cur)]: cur
+    })
+  ), {})
 )
 
-const makeCreators = (provider) =>  {
-    const {config} = provider
-    const uniqueId = config.uniqueId
+const makeCreators = (provider) => {
+  const {config} = provider
+  const uniqueId = config.uniqueId
 
     // HACK: bind all method exported to the provider
      ;['fetch', 'detail', 'random'].map(method => {
-        provider[method] = provider[method].bind(provider)
-    })
+    provider[method] = provider[method].bind(provider)
+  })
 
-    const addToHash = (state, items) => ({
-        ...state,
-        ...hashify(items, (k) => (k[uniqueId]))
-    })
+  const addToHash = (state, items) => ({
+    ...state,
+    ...hashify(items, (k) => (k[uniqueId]))
+  })
 
-    return {
-        FETCH: {
+  return {
+    FETCH: {
 
-            payloadCreator: (syncPayload, dispatch, getState) => {
-                const {filters} = getState()
+      payloadCreator: (syncPayload, dispatch, getState) => {
+        const {filters} = getState()
 
-                return provider.fetch(filters)
-                               .catch(e => Promise.reject('error' + e))
-            },
-            handler: (state, {payload}) => {
-                const {results} = payload
+        return provider.fetch(filters)
+      },
+      handler: (state, {payload}) => {
+        const {results} = payload
 
-                return {
-                    ...state,
-                    cache: addToHash(state.cache, results),
-                    items: results.map(i => i[uniqueId]),
-                    fetched: true
-                }
-            }
-        },
-        DETAIL: {
-            payloadCreator: (id, dispatch, getState) => {
-                const {cache} = getState()
-                return provider.detail(id, cache ? cache[id]: {})
-            },
-            handler: (state, {payload}) => {
-                const id = payload[uniqueId]
-
-                return {
-                    ...state,
-                    cache: addToHash(state.cache, [payload]),
-                    detail: id,
-                }
-
-            }
-        },
-        RANDOM: {
-            payloadCreator: (syncPayload, dispatch, getState) => {
-                return provider.random()
-            },
-            handler: (state, {payload}) => {
-                const id = payload[uniqueId]
-
-                return {
-                    ...state,
-                    cache: addToHash(state.cache, [payload]),
-                    random: id
-                }
-            }
-        },
-        UPDATE: {
-            payloadCreator: (shouldSucceed, dispatch, getState) => (
-                provider.update(shouldSucceed)
-            ),
-            handler: (state, {payload}) => ({
-                ...state,
-                fetched: true,
-                lastUpdated: payload?Date.now():state.lastUpdated
-            })
+        return {
+          ...state,
+          cache: addToHash(state.cache, results),
+          items: results.map(i => i[uniqueId]),
+          fetched: true
         }
+      }
+    },
+    DETAIL: {
+      payloadCreator: (id, dispatch, getState) => {
+        const {cache} = getState()
+        return provider.detail(id, cache ? cache[id] : {})
+      },
+      handler: (state, {payload}) => {
+        const id = payload[uniqueId]
+
+        return {
+          ...state,
+          cache: addToHash(state.cache, [payload]),
+          detail: id
+        }
+      }
+    },
+    RANDOM: {
+      payloadCreator: (syncPayload, dispatch, getState) => {
+        return provider.random()
+      },
+      handler: (state, {payload}) => {
+        const id = payload[uniqueId]
+
+        return {
+          ...state,
+          cache: addToHash(state.cache, [payload]),
+          random: id
+        }
+      }
+    },
+    UPDATE: {
+      payloadCreator: (shouldSucceed, dispatch, getState) => (
+        provider.update(shouldSucceed)
+      ),
+      handler: (state, {payload}) => ({
+        ...state,
+        fetched: true,
+        lastUpdated: payload ? Date.now() : state.lastUpdated
+      })
     }
+  }
 }
 
-const resolveProvider = (provider, config) => {
-    debug(provider)
-    switch(typeof provider) {
-        case 'object':
-            return provider
-            break;
-        case 'function':
-            return new provider(config)
-            break;
-        case 'string':
-        default:
-            const Instance = require(`butter-provider-${provider}`)
-            return new Instance(config)
-    }
+const resolveProvider = (Provider, config) => {
+  debug(Provider)
+  switch (typeof Provider) {
+    case 'object':
+      return Provider
+      break
+    case 'function':
+      return new Provider(config)
+      break
+    case 'string':
+    default:
+      const Instance = require(`butter-provider-${Provider}`)
+      return new Instance(config)
+  }
 }
 
 const makeHandlers = (actionTypes, creators) => {
-    const actionKeys = Object.keys(creators)
+  const actionKeys = Object.keys(creators)
 
-    return actionKeys.reduce((handlers, cur) => {
-        const actionType = actionTypes[cur]
+  return actionKeys.reduce((handlers, cur) => {
+    const actionType = actionTypes[cur]
 
-        const reducer = createReducer()
-            .when(actionType, (state, {type}) => ({
-                ...state,
-                isFetching: type}))
-            .done((state, action) => (
-                creators[cur].handler({
-                    ...state,
-                    isFetching: false
-                }, action)))
-            .failed((state, action) => ({
-                ...state,
-                failed: action,
-                isFetching: false
-            }))
-            .build()
+    const reducer = createReducer()
+      .when(actionType, (state, {type}) => ({
+        ...state,
+        isFetching: type}))
+      .done((state, action) => (
+        creators[cur].handler({
+          ...state,
+          isFetching: false
+        }, action)))
+      .failed((state, action) => ({
+        ...state,
+        failed: action,
+        isFetching: false
+      }))
+      .build()
 
-        return Object.assign(handlers, {
-            [actionType]: reducer,
-            [`${actionType}_COMPLETED`]: reducer,
-            [`${actionType}_FAILED`]: reducer,
-        })
-    }, {})
+    return Object.assign(handlers, {
+      [actionType]: reducer,
+      [`${actionType}_COMPLETED`]: reducer,
+      [`${actionType}_FAILED`]: reducer
+    })
+  }, {})
 }
 
 const makeReducer = (handlers) => {
-    return (state, action) => {
-        const handler = handlers[action.type]
+  return (state, action) => {
+    const handler = handlers[action.type]
 
-        if (handler) {
-            return handler(state, action)
-        }
-
-        return {
-            isFetching: false,
-            fetched: false,
-            detail: null,
-            random: null,
-            lastUpdated: null,
-            items: [],
-            cache: {},
-            ...state,
-        }
+    if (handler) {
+      return handler(state, action)
     }
+
+    return {
+      isFetching: false,
+      fetched: false,
+      detail: null,
+      random: null,
+      lastUpdated: null,
+      items: [],
+      cache: {},
+      ...state
+    }
+  }
 }
 
 const makeActionTypes = (config, creators) => {
-    const actionKeys = Object.keys(creators)
-    const upperName = config.name.toUpperCase()
+  const actionKeys = Object.keys(creators)
+  const upperName = config.name.toUpperCase()
 
-    return actionKeys.reduce((actionTypes, type) => (
-        Object.assign(actionTypes, {
-            [type]: `BUTTER/PROVIDERS/${upperName}/${type}`
-        })
-    ), {})
+  return actionKeys.reduce((actionTypes, type) => (
+    Object.assign(actionTypes, {
+      [type]: `BUTTER/PROVIDERS/${upperName}/${type}`
+    })
+  ), {})
 }
-
 
 const makeActions = (actionTypes, creators) => {
-    return Object.keys(actionTypes).reduce((actions, type) => {
-        const creator = creators[type]
+  return Object.keys(actionTypes).reduce((actions, type) => {
+    const creator = creators[type]
 
-        return Object.assign(actions, {
-            [type]: createAsyncAction(
-                actionTypes[type],
-                creator.payloadCreator
-            )
-        })
-    }, {})
+    return Object.assign(actions, {
+      [type]: createAsyncAction(
+        actionTypes[type],
+        creator.payloadCreator
+      )
+    })
+  }, {})
 }
 
-const reduxProviderAdapter = (providerArg, config = {}) =>  {
-    const provider = resolveProvider(providerArg, config)
+const reduxProviderAdapter = (providerArg, config = {}) => {
+  const provider = resolveProvider(providerArg, config)
 
-    const creators = makeCreators(provider)
-    const actionTypes = makeActionTypes(provider.config, creators)
+  const creators = makeCreators(provider)
+  const actionTypes = makeActionTypes(provider.config, creators)
 
-    return {
-        provider: provider,
-        actionTypes: actionTypes,
-        actions: makeActions(actionTypes, creators),
-        reducer: makeReducer(makeHandlers(actionTypes, creators))
-    }
+  return {
+    provider: provider,
+    actionTypes: actionTypes,
+    actions: makeActions(actionTypes, creators),
+    reducer: makeReducer(makeHandlers(actionTypes, creators))
+  }
 }
 
 export {reduxProviderAdapter as default}
--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -1,126 +1,126 @@
-import expect from 'expect';
+import expect from 'expect'
 
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 
-import configureMockStore from 'redux-mock-store';
-import ButterMockProvider from 'butter-mock-provider';
+import configureMockStore from 'redux-mock-store'
+import ButterMockProvider from 'butter-mock-provider'
 
-import reduxProviderAdapter from '../../src';
+import reduxProviderAdapter from '../../src'
 
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
-const debug = require('debug')('butter-redux-provider:test');
+const debug = require('debug')('butter-redux-provider:test')
 
-const actionKeys = ['FETCH', 'DETAIL', 'RANDOM', 'UPDATE'];
+const actionKeys = ['FETCH', 'DETAIL', 'RANDOM', 'UPDATE']
 
 describe('butter-redux-provider', () => {
-  let MockProviderInstance;
+  let MockProviderInstance
 
   beforeEach(() => {
-    MockProviderInstance = new ButterMockProvider();
-  });
+    MockProviderInstance = new ButterMockProvider()
+  })
 
   const checkReduxProvider = (Provider, name) => {
     const {
       provider, actionTypes, actions
-    } = Provider;
+    } = Provider
 
-    expect(provider).toBeTruthy();
+    expect(provider).toBeTruthy()
 
-    actionKeys.forEach(key => expect(actionTypes).toHaveProperty(key));
-    actionKeys.forEach(key => expect(actions).toHaveProperty(key));
+    actionKeys.forEach(key => expect(actionTypes).toHaveProperty(key))
+    actionKeys.forEach(key => expect(actions).toHaveProperty(key))
 
-    expect(provider.config.name).toEqual(name);
-  };
+    expect(provider.config.name).toEqual(name)
+  }
 
   it('loads a provider by name', () => {
-    checkReduxProvider(reduxProviderAdapter('vodo'), 'vodo');
-  });
+    checkReduxProvider(reduxProviderAdapter('vodo'), 'vodo')
+  })
 
   it('loads a provider by instance', () => {
-    checkReduxProvider(reduxProviderAdapter(ButterMockProvider), 'mock');
-  });
+    checkReduxProvider(reduxProviderAdapter(ButterMockProvider), 'mock')
+  })
 
   it('loads a provider by instanciated object', () => {
-    checkReduxProvider(reduxProviderAdapter(MockProviderInstance), 'mock');
-  });
+    checkReduxProvider(reduxProviderAdapter(MockProviderInstance), 'mock')
+  })
 
   describe('actions', () => {
-    let store;
-    let reduxProvider;
-    const mockProviderInstance = new ButterMockProvider();
+    let store
+    let reduxProvider
+    const mockProviderInstance = new ButterMockProvider()
 
     beforeEach(() => {
-      reduxProvider = reduxProviderAdapter(mockProviderInstance);
-      store = mockStore({items: []});
-    });
+      reduxProvider = reduxProviderAdapter(mockProviderInstance)
+      store = mockStore({items: []})
+    })
 
     it('fetches', () => {
-      const promise = store.dispatch(reduxProvider.actions.FETCH());
-      debug('fetch', store.getState());
+      const promise = store.dispatch(reduxProvider.actions.FETCH())
+      debug('fetch', store.getState())
 
       return promise.then(() => { // return of async actions
-        const actions = store.getActions();
-        const lastAction = actions.pop();
-        const {payload} = lastAction;
+        const actions = store.getActions()
+        const lastAction = actions.pop()
+        const {payload} = lastAction
 
-        debug('got', payload);
+        debug('got', payload)
 
-        expect(lastAction.type).toEqual(`${reduxProvider.actionTypes.FETCH}_COMPLETED`);
-        expect(payload).toHaveProperty('hasMore');
-        expect(payload).toHaveProperty('results');
-        expect(payload.results.length).toEqual(99);
-      });
-    });
+        expect(lastAction.type).toEqual(`${reduxProvider.actionTypes.FETCH}_COMPLETED`)
+        expect(payload).toHaveProperty('hasMore')
+        expect(payload).toHaveProperty('results')
+        expect(payload.results.length).toEqual(99)
+      })
+    })
 
     it('details', (done) => {
       store.dispatch(reduxProvider.actions.DETAIL('42'))
         .then(() => { // return of async actions
-          const actions = store.getActions();
-          const lastAction = actions.pop();
-          const {payload} = lastAction;
+          const actions = store.getActions()
+          const lastAction = actions.pop()
+          const {payload} = lastAction
 
-          expect(lastAction.type).toEqual(`${reduxProvider.actionTypes.DETAIL}_COMPLETED`);
-          expect(payload).toEqual(mockProviderInstance.mockData['42']);
+          expect(lastAction.type).toEqual(`${reduxProvider.actionTypes.DETAIL}_COMPLETED`)
+          expect(payload).toEqual(mockProviderInstance.mockData['42'])
 
-          done();
-        });
-    });
+          done()
+        })
+    })
 
     it('randoms', () => {
-      const hackPayload = {hack: true};
+      const hackPayload = {hack: true}
 
       return store.dispatch(reduxProvider.actions.DETAIL('42', hackPayload))
         .then(() => store.dispatch(reduxProvider.actions.RANDOM()))
         .then(() => { // return of async actions
-          const actions = store.getActions();
-          let lastAction = actions.pop();
+          const actions = store.getActions()
+          let lastAction = actions.pop()
 
-          expect(lastAction.type).toEqual(`${reduxProvider.actionTypes.RANDOM}_COMPLETED`);
+          expect(lastAction.type).toEqual(`${reduxProvider.actionTypes.RANDOM}_COMPLETED`)
 
-          lastAction = actions.pop();
-          expect(lastAction.type).toEqual(reduxProvider.actionTypes.RANDOM);
+          lastAction = actions.pop()
+          expect(lastAction.type).toEqual(reduxProvider.actionTypes.RANDOM)
 
-          lastAction = actions.pop();
-          const {payload} = lastAction;
+          lastAction = actions.pop()
+          const {payload} = lastAction
 
-          expect(lastAction.type).toEqual(`${reduxProvider.actionTypes.DETAIL}_COMPLETED`);
-          expect(payload).toHaveProperty('id');
-          expect(payload).toHaveProperty('title');
-        });
-    });
-  });
+          expect(lastAction.type).toEqual(`${reduxProvider.actionTypes.DETAIL}_COMPLETED`)
+          expect(payload).toHaveProperty('id')
+          expect(payload).toHaveProperty('title')
+        })
+    })
+  })
 
   describe('reducer', () => {
-    let mockProviderInstance;
-    let reduxProvider;
-    let store;
+    let mockProviderInstance
+    let reduxProvider
+    let store
 
     beforeEach(() => {
-      mockProviderInstance = new ButterMockProvider();
-      reduxProvider = reduxProviderAdapter(mockProviderInstance);
+      mockProviderInstance = new ButterMockProvider()
+      reduxProvider = reduxProviderAdapter(mockProviderInstance)
 
       /*
          ;['fetch', 'detail', 'random'].map(method => {
@@ -135,93 +135,93 @@ describe('butter-redux-provider', () => {
          })
        */
 
-      store = createStore(reduxProvider.reducer, applyMiddleware(...middlewares));
-      store.subscribe(() => debug('DISPATCH', store.getState()));
-    });
+      store = createStore(reduxProvider.reducer, applyMiddleware(...middlewares))
+      store.subscribe(() => debug('DISPATCH', store.getState()))
+    })
 
     it('fetches', (done) => {
-      let state = store.getState();
+      let state = store.getState()
 
-      expect(state.isFetching).toEqual(false, 'isFetching before');
-      expect(state.fetched).toEqual(false, 'fetched before');
-      expect(state.lastUpdated).toEqual(null, 'lastUpdated before');
-      expect(state.items).toEqual([], 'items before');
+      expect(state.isFetching).toEqual(false, 'isFetching before')
+      expect(state.fetched).toEqual(false, 'fetched before')
+      expect(state.lastUpdated).toEqual(null, 'lastUpdated before')
+      expect(state.items).toEqual([], 'items before')
 
-      const promise = store.dispatch(reduxProvider.actions.FETCH());
+      const promise = store.dispatch(reduxProvider.actions.FETCH())
 
-      state = store.getState();
+      state = store.getState()
 
-      debug('state after', state);
+      debug('state after', state)
 
-      expect(state.isFetching).toEqual(reduxProvider.actionTypes.FETCH, 'isFetching after');
-      expect(state.fetched).toEqual(false, 'fetched after');
-      expect(state.lastUpdated).toEqual(null, 'lastUpdated after');
-      expect(state.items).toEqual([], 'items after');
+      expect(state.isFetching).toEqual(reduxProvider.actionTypes.FETCH, 'isFetching after')
+      expect(state.fetched).toEqual(false, 'fetched after')
+      expect(state.lastUpdated).toEqual(null, 'lastUpdated after')
+      expect(state.items).toEqual([], 'items after')
 
       promise.then(() => {
-        state = store.getState();
+        state = store.getState()
 
-        debug('state resolved', state);
+        debug('state resolved', state)
 
-        expect(state.isFetching).toEqual(false, 'isFetching resloved');
-        expect(state.fetched).toEqual(true, 'fetched resolved');
+        expect(state.isFetching).toEqual(false, 'isFetching resloved')
+        expect(state.fetched).toEqual(true, 'fetched resolved')
 
-        const {items} = state;
+        const {items} = state
 
-        expect(items.length).toEqual(99, 'items length resolved');
-        expect(Object.keys(state.cache)).toEqual(Array.from(Array(99)).map((e, i) => `${i}`), 'cache keys resolved');
-        expect(Object.keys(state.cache).length).toEqual(99, 'cache length resolved');
-        done();
-      });
-    });
+        expect(items.length).toEqual(99, 'items length resolved')
+        expect(Object.keys(state.cache)).toEqual(Array.from(Array(99)).map((e, i) => `${i}`), 'cache keys resolved')
+        expect(Object.keys(state.cache).length).toEqual(99, 'cache length resolved')
+        done()
+      })
+    })
 
     it('details', (done) => {
-      let state = store.getState();
+      let state = store.getState()
 
-      expect(state.isFetching).toEqual(false, 'isFetching before');
-      expect(state.fetched).toEqual(false, 'fetched before');
-      expect(state.lastUpdated).toEqual(null, 'lastUpdated before');
-      expect(state.items).toEqual([], 'items before');
+      expect(state.isFetching).toEqual(false, 'isFetching before')
+      expect(state.fetched).toEqual(false, 'fetched before')
+      expect(state.lastUpdated).toEqual(null, 'lastUpdated before')
+      expect(state.items).toEqual([], 'items before')
 
-      const promise = store.dispatch(reduxProvider.actions.DETAIL('42'));
-      state = store.getState();
+      const promise = store.dispatch(reduxProvider.actions.DETAIL('42'))
+      state = store.getState()
 
-      debug('state after', state);
+      debug('state after', state)
 
-      expect(state.isFetching).toEqual(reduxProvider.actionTypes.DETAIL, 'isFetching after');
-      expect(state.fetched).toEqual(false, 'fetched after');
-      expect(state.lastUpdated).toEqual(null, 'lastUpdated after');
-      expect(state.items).toEqual([], 'items after');
+      expect(state.isFetching).toEqual(reduxProvider.actionTypes.DETAIL, 'isFetching after')
+      expect(state.fetched).toEqual(false, 'fetched after')
+      expect(state.lastUpdated).toEqual(null, 'lastUpdated after')
+      expect(state.items).toEqual([], 'items after')
 
       promise.then(() => { // return of async actions
-        state = store.getState();
+        state = store.getState()
 
-        expect(state.isFetching).toEqual(false, 'isFetching resolved');
+        expect(state.isFetching).toEqual(false, 'isFetching resolved')
 
-        expect(state.detail).toEqual(42);
-        expect(state.cache[42]).toBeTruthy();
-        expect(state.cache[42]).toHaveProperty('id');
-        expect(state.cache[42]).toHaveProperty('synopsis');
+        expect(state.detail).toEqual(42)
+        expect(state.cache[42]).toBeTruthy()
+        expect(state.cache[42]).toHaveProperty('id')
+        expect(state.cache[42]).toHaveProperty('synopsis')
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('randoms', () => store.dispatch(reduxProvider.actions.DETAIL('42'))
       .then(() => store.dispatch(reduxProvider.actions.RANDOM()))
       .then((payload) => { // return of async actions
-        expect(payload).toHaveProperty('id');
-        expect(payload).toHaveProperty('title');
-      }));
+        expect(payload).toHaveProperty('id')
+        expect(payload).toHaveProperty('title')
+      }))
 
     it('update', () => store.dispatch(reduxProvider.actions.UPDATE())
       .then((payload) => { // return of async actions
-        expect(payload.length).toEqual(99);
-      }));
+        expect(payload.length).toEqual(99)
+      }))
 
     it('update fail', () => store.dispatch(reduxProvider.actions.UPDATE(false))
       .then((payload) => { // return of async actions
-        expect(payload).toEqual(null);
-      }));
-  });
-});
+        expect(payload).toEqual(null)
+      }))
+  })
+})
